93bd9928edc112e33251e27be12d718215623092
Asad Khan <asad_khan47@hotmail.co.uk>
2019-02-04 10:11:08 +0000

    Able to add more than 1 birthday

--- spec/birthday_spec.rb
+++ spec/birthday_spec.rb
@@ -6,4 +6,9 @@ describe Birthday do
   it 'can store a name along with a birthday' do
     expect(birthday.add("Asad", "23 June 1994")).to eq([{name: "Asad", birthday: "23 June 1994"}])
   end
+
+  it 'can store 2 peoples names and birthdays' do
+    birthday.add("Asad", "23 June 1994")
+    expect(birthday.add("Glass", "12 December 1994")).to eq([{name: "Asad", birthday: "23 June 1994"}, {name: "Glass", birthday: "12 December 1994"}])
+  end
 end
