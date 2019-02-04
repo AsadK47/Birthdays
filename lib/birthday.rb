4642e7c100598ebb0534251dac78d924ab8abe6b
Asad Khan <asad_khan47@hotmail.co.uk>
2019-02-04 10:47:57 +0000

    Can print birthdays in a nice format

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,4 +6,10 @@ class Birthday
   def add(name, birthdate)
     @list << { name: name, birthday: birthdate }
   end
+
+  def show
+    @list.each do |hash|
+      puts "#{hash[:name]}: #{hash[:birthday]}"
+    end
+  end
 end
--- spec/birthday_spec.rb
+++ spec/birthday_spec.rb
@@ -4,11 +4,18 @@ describe Birthday do
   subject(:birthday) { described_class.new }
 
   it 'can store a name along with a birthday' do
-    expect(birthday.add("Asad", "23 June 1994")).to eq([{name: "Asad", birthday: "23 June 1994"}])
+    expect(birthday.add("Leonardo", "11 November 1974")).to eq([{name: "Leonardo", birthday: "11 November 1974"}])
   end
 
   it 'can store 2 peoples names and birthdays' do
-    birthday.add("Asad", "23 June 1994")
-    expect(birthday.add("Glass", "12 December 1994")).to eq([{name: "Asad", birthday: "23 June 1994"}, {name: "Glass", birthday: "12 December 1994"}])
+    birthday.add("Leonardo", "11 November 1974")
+    expect(birthday.add("Glass", "12 December 1994")).to eq([{name: "Leonardo", birthday: "11 November 1974"}, {name: "Glass", birthday: "12 December 1994"}])
+  end
+
+  it 'can print the birthdays' do
+    birthday.add("Leonardo", "11 November 1974")
+    birthday.add("Glass", "12 December 1994")
+    expect { birthday.show }.to output("Leonardo: 11 November 1974\n"\
+      "Glass: 12 December 1994\n").to_stdout
   end
 end
