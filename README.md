fdd532cfb55cd7f714a3801a49279e2af99179aa
Asad Khan <asad_khan47@hotmail.co.uk>
2019-02-04 15:19:41 +0000

    Added README and rubocop'd

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Birthdays
+
+Simple app to add birthdays and list them
+
+## How to run
+
+Once you have forked/cloned the repo, cd in Birthdays and require the file in an irb as shown below
+
+```
+irb
+require "./lib/birthday"
+birthdays = Birthday.new
+birthday.add("Leonardo", "11 November 1974")
+birthday.add("Barack", "4 August 1961")
+```
+
+## Screenshot of app in action
+
+![](assets/README-ebf19e28.png)
+
+## Running tests and linter
+
+The following commands must be run in the root directory of the project
+
+Testing: ```rspec```
+Linter: ```rubocop```
--- spec/birthday_spec.rb
+++ spec/birthday_spec.rb
@@ -4,12 +4,12 @@ describe Birthday do
   subject(:birthday) { described_class.new }
 
   it 'can store a name along with a birthday' do
-    expect(birthday.add("Leonardo", "11 November 1974")).to eq([{name: "Leonardo", birthday: "11 November 1974"}])
+    expect(birthday.add("Leonardo", "11 November 1974")).to eq([{ name: "Leonardo", birthday: "11 November 1974" }])
   end
 
   it 'can store 2 peoples names and birthdays' do
     birthday.add("Leonardo", "11 November 1974")
-    expect(birthday.add("Glass", "12 December 1994")).to eq([{name: "Leonardo", birthday: "11 November 1974"}, {name: "Glass", birthday: "12 December 1994"}])
+    expect(birthday.add("Glass", "12 December 1994")).to eq([{ name: "Leonardo", birthday: "11 November 1974" }, { name: "Glass", birthday: "12 December 1994" }])
   end
 
   it 'can print the birthdays' do
